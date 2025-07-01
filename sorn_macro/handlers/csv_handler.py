ba0bf9083a88b98766417fc657e1dc1bdbfdd29f
Korreq <lukasz2600@gmail.com>
2025-07-01 15:29:40 +0200

    Changed formatting in elements' files

--- sorn_macro/handlers/csv_handler.py
+++ sorn_macro/handlers/csv_handler.py
@@ -1,4 +1,5 @@
 import csv
+import locale
 
 from elements.functions import ElementsFunctions
 from core.psat_functions import PsatFunctions
@@ -9,11 +10,8 @@ class CsvFile:
     def __init__(self, path, timestamp='', prefix=''):
 
         self.functions = ElementsFunctions()
-
         self.psat = PsatFunctions()
-
         self.path = path
-
         self.name_addons = ''
 
         if timestamp:
@@ -32,57 +30,55 @@ class CsvFile:
         with open(full_path, 'w', newline='') as file:
 
             writer = csv.writer(file, dialect="excel-semicolon")
-
             writer.writerow(header)
-
             writer.writerows(rows)
 
 
     def write_buses_file(self, buses):
 
         header = ["Name", "Type", "KV"]
-
         rows = []
 
         for bus in buses:
 
-            rows.append( [ bus.name[:-4].strip(), self.functions.get_bus_type(bus), self.functions.get_bus_changed_kv_vmag(bus,0)[1] ] )
+            rows.append( [ bus.name[:-4].strip(), self.functions.get_bus_type(bus), 
+                          locale.format_string('%G', self.functions.get_bus_changed_kv_vmag(bus,0)[1]) ] )
           
         self.write_to_file("buses", header, rows)
 
 
     def write_gens_file(self, generators):
-
+        
         header = ["Bus", "Name", "MW min", "Current MW", "MW max", "Mvar min", "Current Mvar", "Mvar max"]
-      
         rows = []
 
         for gen in generators:
 
-            rows.append( [ self.functions.get_bus_name_from_id(gen.bus), gen.eqname, gen.mwmin, gen.mw, gen.mwmax, 
-                 gen.mvarmin, gen.mvar, gen.mvarmax ] )
+            rows.append( [ self.functions.get_bus_name_from_id(gen.bus), gen.eqname, locale.format_string('%G', gen.mwmin), 
+                        locale.format_string('%G', gen.mw), locale.format_string('%G', gen.mwmax), 
+                        locale.format_string('%G', gen.mvarmin), locale.format_string('%G', gen.mvar), 
+                        locale.format_string('%G', gen.mvarmax) ] )
             
         self.write_to_file("generators", header, rows)
 
 
     def write_shunts_file(self, shunts):
-
+        
         header = ["Bus", "Name", "Status", "Nominal MW", "MW", "Nominal Mvar", "Mvar"]
-
         rows = []
 
         for shunt in shunts:
 
-            rows.append( [ self.functions.get_bus_name_from_id(shunt.bus), shunt.eqname, shunt.status, shunt.nommw, 
-                          shunt.mw, shunt.nommvar, shunt.mvar ] )
+            rows.append( [ self.functions.get_bus_name_from_id(shunt.bus), shunt.eqname, shunt.status, 
+                        locale.format_string('%G', shunt.nommw), locale.format_string('%G', shunt.mw), 
+                        locale.format_string('%G', shunt.nommvar), locale.format_string('%G', shunt.mvar) ] )
 
         self.write_to_file("shunts", header, rows)
 
     
     def wrtie_trfs_file(self, transformers, margin):
-
+        
         header = ["From Bus", "To Bus", "Name", "Current tap", "Max tap"]
-
         rows = []
 
         for trf in transformers:
@@ -91,7 +87,6 @@ class CsvFile:
             end_bus = self.psat.get_bus_data(trf.tobus)
 
             current_tap, max_tap = self.functions.get_transformer_ratios(trf, beg_bus.basekv, end_bus.basekv, margin, True)
-
             rows.append( [ self.functions.get_bus_name_from_id(trf.frbus), self.functions.get_bus_name_from_id(trf.tobus), 
                           trf.name, current_tap, max_tap ] )
             
