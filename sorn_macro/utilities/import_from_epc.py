ff07a4986081ea9aed9b3bff319402282c9be747
Korreq <lukasz2600@gmail.com>
2025-08-28 12:31:30 +0200

    Delete .epc file after import

--- sorn_macro/utilities/import_from_epc.py
+++ sorn_macro/utilities/import_from_epc.py
@@ -1,4 +1,5 @@
 from core.psat_functions import PsatFunctions
+
 from handlers.file_handler import FileHandler
 
 class ImportFromEPC:
@@ -13,6 +14,12 @@ class ImportFromEPC:
         file_path = FileHandler.find_file_in_directory(directory, model_name)
         if file_path:
             self.psat.import_epc(file_path)
+            
+            #Change file extension to .epc if needed 
+            epc_model_name = model_name if model_name.endswith('.epc') else model_name.split('.')[0] + '.epc'
+
+            #Delete if found model with .epc extension
+            FileHandler.delete_files_from_directory(directory, epc_model_name)
             return f"Updated pfb file with: {file_path}"
 
         return f"Used original file: {model_name}"
